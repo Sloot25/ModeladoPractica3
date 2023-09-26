614f43e4f367bc85bb4712c6591c39d8d4dd1fea
Carlos Razo <Konquistad@gmail.com>
2023-09-25 23:25:49 -0600

    ahora se puede equipar un soldado con infinidad de armas repetidas, tambien corregi el readme

--- src/EquiparNuevoSoldado.java
+++ src/EquiparNuevoSoldado.java
@@ -21,10 +21,8 @@ public class EquiparNuevoSoldado {
         System.out.println("Indique el nombre del soldado");
         nombre = sc.nextLine();
         Soldado nuevoSoldado = escogerRango(nombre);
-        nuevoSoldado = agregarArmadura(nuevoSoldado);
-        nuevoSoldado = agregarArma(nuevoSoldado);
-        nuevoSoldado = agregarComplemento(nuevoSoldado);
         System.out.println(nuevoSoldado);
+        agregarArma(nuevoSoldado);
     } 
 
     /*
@@ -70,20 +68,27 @@ public class EquiparNuevoSoldado {
     }
 
     /*
-     * Metodo que pregunta la armadura que portara su soldado (opcional)
+     * Metodo que pregunta el arma que portara su soldado (opcional)
      * 
      * @param nombre: Recibe el soldado instanceado en el metodo anterior
      * 
      * @return soldado: Regresa un soldado decorado con la armadura
      */
-    public Soldado agregarArmadura(Soldado soldado){
+    public void agregarArma(Soldado soldado){
+        int contador_armas = 0;
         do{
             String menu = "Indique la armadura que desea equipar.\n" +
                         "1.- Armadura de kevlar.\n" +
                         "2.- Armadura de grafeno.\n" +
                         "3.- Armadura de tanque.\n" +
                         "4.- Iron Cheems.\n" +
-                        "0.- Sin armadura";
+                        "5.- Espada gigante de anime.\n" +
+                        "6.- Pistola de papas.\n" +
+                        "7.- Rasho laser.\n" +
+                        "8.- Alas de gallina.\n" +
+                        "9.- Piernas de rana gigante.\n" +
+                        "10.- Turbinas.\n" +
+                        "0.- Finalizar";
             System.out.println(menu);
             while (true) {
                 try {
@@ -96,115 +101,93 @@ public class EquiparNuevoSoldado {
             }
             switch (opcion) {
                 case 1:
-                    return new ArmaduraKevlar(soldado);
-        
+                    contador_armas += 1; 
+                    soldado = new ArmaduraKevlar(soldado);
+                    System.out.println("REPORTE DE SOLDADO:\n");
+                    System.out.println(soldado);
+                    break;
+
                 case 2:
-                    return new ArmaduraGrafeno(soldado);
+                    contador_armas += 1;
+                    soldado = new ArmaduraGrafeno(soldado);
+                    System.out.println("REPORTE DE SOLDADO:\n");
+                    System.out.println(soldado);
+                    break;
 
                 case 3:
-                    return new ArmaduraTanque(soldado);
+                    contador_armas += 1;
+                    soldado = new ArmaduraTanque(soldado);
+                    System.out.println("REPORTE DE SOLDADO:\n");
+                    System.out.println(soldado);
+                    break;
                 
                 case 4:
                     if (soldado.getRango().equals("Capitan")){
-                        return new IronCheems(soldado); 
+                        contador_armas += 1;
+                        soldado = new IronCheems(soldado); 
+                        System.out.println("REPORTE DE SOLDADO:\n");
+                        System.out.println(soldado);
+                        break;
                     } else{
                         System.out.println("El soldado no esta autorizado para portar esta armadura.");
+                        continue;
                     }
-                case 0:
-                    System.out.println("Soldado sin armadura...");
+                case 5:
+                    contador_armas += 1;
+                    soldado = new EspadaGiganteAnime(soldado);
+                    System.out.println("REPORTE DE SOLDADO:\n");
+                    System.out.println(soldado);
                     break;
+        
+                case 6:
+                    contador_armas += 1;
+                    soldado = new PistolaPapas(soldado);
+                    System.out.println("REPORTE DE SOLDADO:\n");
+                    System.out.println(soldado);
+                    break;
+
+                case 7:
+                    contador_armas += 1;
+                    soldado = new RashoLaser(soldado);
+                    System.out.println("REPORTE DE SOLDADO:\n");
+                    System.out.println(soldado);
+                    break;
+
+                case 8:
+                    contador_armas += 1;
+                    soldado = new AlasGallina(soldado);
+                    System.out.println("REPORTE DE SOLDADO:\n");
+                    System.out.println(soldado);
+                    break;
+
+                case 9:
+                    contador_armas += 1;
+                    soldado = new PiernasRanaGigante(soldado);
+                    System.out.println("REPORTE DE SOLDADO:\n");
+                    System.out.println(soldado);
+                    break;
+
+                case 10:
+                    contador_armas += 1;
+                    soldado = new Turbinas(soldado);
+                    System.out.println("REPORTE DE SOLDADO:\n");
+                    System.out.println(soldado);
+                    break;
+
+                case 0:
+                    if (contador_armas == 0){
+                        System.out.println("Tu soldado no puede ir desarmado a la guerra. No seas cruel :c.");
+                        continue;
+                    } else{
+                        System.out.println("REPORTE FINAL DE SOLDADO:\n");
+                        System.out.println(soldado);
+                        opcion = -1;
+                        break;
+                    }
+
                 default:
                 System.out.println("Opcion no valida");
             }
-        }while(opcion != 0);
-        return soldado;
+        } while(opcion != -1);
     }
-
-    /*
-     * Metodo que pregunta el arma que portara su soldado (obligatorio ya que
-     * ningun soldado debe ir desarmado)
-     * 
-     * @param nombre: Recibe el soldado instanceado en el metodo anterior
-     * 
-     * @return soldado: Regresa un soldado decorado con rl arma
-     */
-    public Soldado agregarArma(Soldado soldado){
-        boolean booleano = true;
-        System.out.println("Indique el arma que desea equipar. (Obligatorio).");
-        while (booleano){
-            System.out.println("1.- Espada gigante de anime.\n" +
-                            "2.- Pistola de papas.\n" +
-                            "3.- Rasho laser.\n");
-             
-            try {
-                String opcionUsuario = sc.nextLine();
-                opcion = Integer.parseInt(opcionUsuario);
-            } catch (NumberFormatException ex){ 
-                System.out.println("Por favor elige una opcion VALIDA:");
-                continue;
-            }
-
-            switch (opcion) {
-                case 1:
-                    return new EspadaGiganteAnime(soldado);
-        
-                case 2:
-                    return new PistolaPapas(soldado);
-
-                case 3:
-                    return new RashoLaser(soldado);
-
-                default:
-                    throw new IllegalArgumentException("Opcion no válida");  
-            }
-        }
-        return soldado;
-    }
-
-    /*
-     * Metodo que pregunta el complemento que portara su soldado (opcional)
-     * 
-     * @param nombre: Recibe el soldado instanceado en el metodo anterior
-     * 
-     * @return soldado: Regresa un soldado decorado con el complemento
-     */
-    public Soldado agregarComplemento(Soldado soldado){
-        do{
-            String menu = "Indique el complemeto que desea equipar.\n" +
-                        "1.- Alas de gallina.\n" +
-                        "2.- Piernas de rana gigante.\n" +
-                        "3.- Turbinas.\n" +
-                        "0.- Sin complemento";
-            System.out.println(menu);
-            while (true) {
-                try {
-                    String opcionUsuario = sc.nextLine();
-                    opcion = Integer.parseInt(opcionUsuario);
-                    break;
-                } catch (NumberFormatException ex){ 
-                    System.out.println(menu);
-                }
-            }
-
-            switch (opcion) {
-                case 1:
-                    return new AlasGallina(soldado);
-        
-                case 2:
-                    return new PiernasRanaGigante(soldado);
-
-                case 3:
-                    return new Turbinas(soldado);
-
-                case 0:
-                    System.out.println("Soldado sin complemento..."); 
-                    return soldado;
-
-                default:
-                    throw new IllegalArgumentException("Opcion no válida");
-            }
-        } while(opcion != 0);
-    }
-
-
 }
